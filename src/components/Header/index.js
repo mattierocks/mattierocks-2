702012e3e513881f5db37fc45c004acbb5d52d0e
mattierocks <mattierocks@gmail.com>
2018-03-06 13:21:59 -0500

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+import logo from '../../images/logo.png'
+
+const HeaderWrapper = styled.div`
+  background: whitesmoke;
+  margin-bottom: 1.45rem;
+  h1 {
+    img {
+      height: 80px;
+    }
+  }
+`
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  maxWidth: 960;
+  padding: 1.45rem 1.0875rem;
+`
+
+
+const Header = () => (
+  <HeaderWrapper>
+    <div>
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'white',
+            textDecoration: 'none',
+          }}
+        >
+          <img src={logo} alt="Mattierocks Logo"/>
+        </Link>
+      </h1>
+    </div>
+    <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+        </ul>
+    </nav>
+  </HeaderWrapper>
+)
+
+export default Header
---
+++ src/pages/blog.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Blog extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Mattie's Blog</h1>
+                <p>
+                Lorem ipsum dolor amet scenester shabby chic viral, health goth occupy fixie forage typewriter. Vaporware slow-carb pickled retro, helvetica biodiesel butcher hella. Gentrify next level irony vice pop-up freegan. Everyday carry kombucha yuccie narwhal. Tilde paleo polaroid ethical four dollar toast air plant mustache lyft fixie edison bulb chillwave banjo. Meditation chartreuse air plant YOLO migas skateboard, knausgaard sustainable. Copper mug pinterest neutra actually.
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Blog

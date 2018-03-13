b98fccea4519d53b5f4473fb3a3621f6104d7503
mattierocks <mattierocks@gmail.com>
2018-03-12 21:15:10 -0400

    update some metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteMetadata: {
-        title: 'Gatsby Default Starter',
-        desc: 'Mattierocks'
+        title: 'Mattierocks.com',
+        desc: 'Wuss Rock!'
     },
     plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components'],
 };
